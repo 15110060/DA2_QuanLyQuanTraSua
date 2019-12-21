8e3ea8bfc318a7d4cee97757e9768db9f546463e
Đặng Tiến Hưng <32192725+15110060@users.noreply.github.com>
2019-12-21 15:02:24 +0700

    21/12/2019

--- project/server.js
+++ project/server.js
@@ -5,22 +5,21 @@ const path = require('path');
 app.use(express.static(__dirname + '/view'));
 //
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/view/home.html'));
-})
+	res.sendFile(path.join(__dirname, '/view/home.html'));
+});
 
 app.get('/detail', (req, res) => {
-    res.sendFile(path.join(__dirname + '/view/detail.html'));
-})
+	res.sendFile(path.join(__dirname + '/view/detail.html'));
+});
 
 app.get('/control', (req, res) => {
-    res.sendFile(path.join(__dirname + '/view/control2.html'));
-})
+	res.sendFile(path.join(__dirname + '/view/control.html'));
+});
 
-app.get('/help', (req, res) => {
-    res.sendFile(path.join(__dirname + '/view/help.html'));
-})
-
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname + '/view/about.html'));
+});
 
 app.listen(3000, () => {
-    console.log("Connection to PORT 3000!");
-})
+	console.log('Connection to PORT 3000!');
+});
